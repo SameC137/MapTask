1d517881999ea852ea7be3968d9d3e965e1f56c9
SameC137 <37704662+SameC137@users.noreply.github.com>
2019-01-12 06:43:27 +0300

    Update App.js

--- App.js
+++ App.js
@@ -10,13 +10,13 @@ constructor(props) {
     this.state={
       markers:[
           {
-            id:1, //Key
+            id:1, //Marker ID
             title:"Title1", //Title
             latlng:{  //Coordinates
               latitude: 37.58825,
               longitude: -122.4324,
             },
-            color:'green' //Color For our Marker
+            color:'green' //Color
           },
           {
             id:2,
@@ -31,11 +31,61 @@ constructor(props) {
             id:3,
             title:"Title3",
             latlng:{  
-              latitude: 37.38825,
-              longitude: -122.5324,
+              latitude: 34.38825,
+              longitude: -120.5324,
             },
             color:'red'
-          }
+          },
+
+          {
+            id:4,
+            title:"Title4",
+            latlng:{  
+              latitude: 25.38825,
+              longitude: -120.5324,
+            },
+            color:'blue'
+          },
+            {
+              id:5,
+              title:"Title5",
+              latlng:{  
+                latitude: 25.38825,
+                longitude: -119.5324,
+              },
+              color:'red'
+            },
+  
+            {
+              id:6,
+              title:"Title6",
+              latlng:{  
+                latitude: 22.38825,
+                longitude: -120.5324,
+              },
+              color:'blue'
+            },
+            
+            {
+              id:6,
+              title:"Title6",
+              latlng:{  
+                latitude: 29.38825,
+                longitude: -126.5324,
+              },
+              color:'blue'
+            },
+    
+              {
+                id:7,
+                title:"Title7",
+                latlng:{  
+                  latitude: 27.38825,
+                  longitude: -120.5324,
+                },
+                color:'blue'
+              }
+
        ]
     };
   }
